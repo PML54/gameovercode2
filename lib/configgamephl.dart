56c19f3b392afc87d6b1ecc7a92831fdb52d86a3
Paul de Pontoise <35325632+PML54@users.noreply.github.com>
2022-08-28 19:03:23 +0200

    Add files via upload

---
+++ lib/configgamephl.dart
@@ -0,0 +1,39 @@
+//220701  Ajout du Path pour la PreProd
+
+import 'dart:core';
+import 'package:flutter/material.dart';
+
+const String prefixPhoto = "upoad/PML_01_"; // Syntaxe
+const String pathPHP= "https://lamemopole.com/php/"; // PROD
+//const String pathPHP = "https://www.paulbrode.com/php/"; //DEV
+
+const String unknownCodeMaster = "Code Incorrect";
+List statusGame = [
+  "Attente du départ",
+  "Caption  en cours",
+  "Fin des Captions",
+  "Vote en cours",
+  "Fin des Votes",
+  "Résultats",
+      "Aborted"
+];
+List statusGU = [
+  "Attend",
+  "Commente",
+  "a Commenté",
+  "Vote",
+  "a Voté",
+  "Resultats",
+  "Aborted"
+];
+List modeGame = ["PUBLIC", "PRIVATE"];
+List msgNewGame = ["Nom Game ?", "Photos Selected ? "];
+List statusUser = ["DISABLED", "ENABLED"];
+Color colorOK = Colors.green;
+Color colorKO = Colors.red;
+//String medalGold=""🥇 🥈 🥉";
+List medals=[ "🥇","🥇" ,"🥈","🥉"];
+String medalGold="🥇";
+String medalSilver="🥈";
+String medalBronze="🥉";
+
